ede2dc47c5f6c4609a892864fc850df74b28f903
Adiguna7 <sevensag@gmail.com>
2020-06-26 06:36:10 +0700

    add docker setup

--- templatemvc/app/config/config.php
+++ templatemvc/app/config/config.php
@@ -4,6 +4,6 @@ define('BASEURL', 'http://localhost/sewaja/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '123');
-define('DB_NAME', 'sewaja');
+define('DB_NAME', 'b201');
 
 ?>
